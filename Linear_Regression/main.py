9452e09d928b3dd0ec5c5f876984fc773288d21f
Ask149 <ashishkshirsagar10@gmail.com>
2019-01-19 17:16:46 +0530

    Restructured the code to add main

--- Linear_Regression/main.py
+++ Linear_Regression/main.py
@@ -1,4 +1,9 @@
 from modules import readTrainData,fit
-x, y = readTrainData()
-a, b = fit(x,y,1,5,10000,0.02)
-print(a,b)
+
+if __name__=="__main__":
+	
+	x, y = readTrainData()
+	
+	a, b = fit(x,y,1,5,10000,0.02)
+	
+	print(a,b)
