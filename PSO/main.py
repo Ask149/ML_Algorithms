6e30fc046c887ec3e06dc6bba1dad659ac25b610
Ask149 <ashishkshirsagar10@gmail.com>
2019-01-20 23:42:06 +0530

    Added comments and restructured the code

--- PSO/main.py
+++ PSO/main.py
@@ -7,12 +7,18 @@
 from pso import PSO
 from particle import Particle
 
+#--------------------------------------------------------------------------
+#
+# Cost function to optimise
 def func(x):
 	total = 0
 	for i in range(len(x)):
 		total += x[i]**2
 	return total
+#
+#--------------------------------------------------------------------------
 
-initial = [5,5] #starting location
-bounds = [(-10,10),(-10,10)] #x_min,x_max pair for each particle
-PSO(func,initial,bounds,particles=15,maxiter=30)
+if __name__ == "__main__":
+  initial = [5,5]              #starting location
+  bounds = [(-10,10),(-10,10)] #x_min, x_max pair for each particle
+  PSO(func,initial,bounds,particles=15,maxiter=30)
--- PSO/particle.py
+++ PSO/particle.py
@@ -2,22 +2,18 @@ import random
 
 class Particle:
 	def __init__(self,pos_0,dimensions):
-		self.pos = []
-		self.vel = []
-		self.pos_best = []
-		self.err_best = -1
-		self.err = -1
+		self.pos = []        # position of particle
+		self.vel = []        # velocity of particle
+		self.pos_best = []   # best position of particle
+		self.err_best = -1   # minimum individual error
+		self.err = -1        # individual error
 
+    # initialize the velocity and position of individual
 		for i in range(0,dimensions):
 			self.vel.append(random.uniform(-1,1))
 			self.pos.append(pos_0[i])
 
-	def evaluate(self,cost,dimensions):
-		self.err = cost(self.pos)
-		if self.err < self.err_best or self.err_best == -1:
-			self.pos_best = self.pos
-			self.err_best = self.err
-
+	# update the velocity of the individual particle
 	def vel_update(self,swarm_pos_best,dimensions):
 		w = 0.4
 		c1 = 1
@@ -30,8 +26,16 @@ class Particle:
 			vel_c2 = c2*r2*(swarm_pos_best[i]-self.pos[i])
 			self.vel[i] = w*self.vel[i] + vel_c1 + vel_c2
 
+  # update the position of the individual particle
 	def pos_update(self,bound,dimensions):
 		for i in range(0,dimensions):
 			self.pos[i] = self.pos[i] + self.vel[i]
 			self.pos[i] = min( bound[i][1],self.pos[i] )
 			self.pos[i] = min( bound[i][0],self.pos[i] )
+
+  # calculate the error ( cost ) of individual particle
+	def evaluate(self,cost,dimensions):
+		self.err = cost(self.pos)
+		if self.err < self.err_best or self.err_best == -1:
+			self.pos_best = self.pos
+			self.err_best = self.err
